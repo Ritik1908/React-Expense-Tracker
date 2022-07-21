7ea75ef2b0b5c441d7bd45249377d689adf377d9
Ritik Verma <ritikverma8100775754@gmail.com>
2022-07-21 16:01:51 +0530

    Expense Form added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,47 @@
-import './Expenses.css'
-import Card from '../UI/Card';
+import React, { useState } from "react";
+
+import "./Expenses.css";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 
 function Expenses(props) {
+
+  const [filteredYear, setFilteredYear] = useState("2020");
+ 
+  const onFilterHandler = (selectedYear) => {
+    console.log("In Expense");
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
+
   return (
-    <Card className='expenses'>
-      <ExpenseItem
-        date={props.items[0].date}
-        amount={props.items[0].amount}
-        title={props.items[0].title}
-      />
-      <ExpenseItem
-        date={props.items[1].date}
-        amount={props.items[1].amount}
-        title={props.items[1].title}
-      />
-      <ExpenseItem
-        date={props.items[2].date}
-        amount={props.items[2].amount}
-        title={props.items[2].title}
-      />
-      <ExpenseItem
-        date={props.items[3].date}
-        amount={props.items[3].amount}
-        title={props.items[3].title}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter selected={filteredYear} onFilter={onFilterHandler} />
+        <ExpenseItem
+          date={props.items[0].date}
+          amount={props.items[0].amount}
+          title={props.items[0].title}
+        />
+        <ExpenseItem
+          date={props.items[1].date}
+          amount={props.items[1].amount}
+          title={props.items[1].title}
+        />
+        <ExpenseItem
+          date={props.items[2].date}
+          amount={props.items[2].amount}
+          title={props.items[2].title}
+        />
+        <ExpenseItem
+          date={props.items[3].date}
+          amount={props.items[3].amount}
+          title={props.items[3].title}
+        />
+      </Card>
+    </div>
   );
 }
 
-export default Expenses
+export default Expenses;
