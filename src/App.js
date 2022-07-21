fe32d8bea2bd45b6de7ae8610e091e84fa9ad6ab
Ritik Verma <ritikverma8100775754@gmail.com>
2022-07-21 17:29:48 +0530

    Chart-Added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React, { useState } from "react";
+
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
       id: "e1",
       title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { 
+      id: "e2", 
+      title: "New TV", 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) 
+    },
     {
       id: "e3",
       title: "Car Insurance",
@@ -24,10 +31,13 @@ function App() {
     },
   ];
 
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
-  }
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,43 +2,28 @@ import React, { useState } from "react";
 
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
-
   const [filteredYear, setFilteredYear] = useState("2020");
- 
+
   const onFilterHandler = (selectedYear) => {
-    console.log("In Expense");
     setFilteredYear(selectedYear);
-    console.log(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
 
   return (
     <div>
       <Card className="expenses">
         <ExpenseFilter selected={filteredYear} onFilter={onFilterHandler} />
-        <ExpenseItem
-          date={props.items[0].date}
-          amount={props.items[0].amount}
-          title={props.items[0].title}
-        />
-        <ExpenseItem
-          date={props.items[1].date}
-          amount={props.items[1].amount}
-          title={props.items[1].title}
-        />
-        <ExpenseItem
-          date={props.items[2].date}
-          amount={props.items[2].amount}
-          title={props.items[2].title}
-        />
-        <ExpenseItem
-          date={props.items[3].date}
-          amount={props.items[3].amount}
-          title={props.items[3].title}
-        />
+        <ExpensesChart expenses = {filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
